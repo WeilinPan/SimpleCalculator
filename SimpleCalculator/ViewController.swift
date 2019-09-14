3afe1a8faed4f1d1f59e4e93d00d74ab32560d75
apan <s71440@gmail.com>
2019-09-14 15:31:31 +0800

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by APAN on 2019/9/14.
+//  Copyright © 2019 APAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
